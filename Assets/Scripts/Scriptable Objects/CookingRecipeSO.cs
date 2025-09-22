d5ce7bee3e99d5b00dc03d2d8d04b1ad137676ec
Loren Penny <loren_667@hotmail.com>
2025-09-22 16:32:25 +0100

    Project reset
    Converting to a 2.5D game

--- Assets/Scripts/Scriptable Objects/CookingRecipeSO.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "CookingRecipeSO", menuName = "Scriptable Objects/CookingRecipeSO")]
-public class CookingRecipeSO : ScriptableObject
-{
-    public KitchenObjectSO input;
-    public KitchenObjectSO output;
-    public float maxCookingTime;
-}
--- Assets/Scripts/Scriptable Objects/KitchenObjectSO.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "KitchenObjectSO", menuName = "Scriptable Objects/KitchenObjectSO")]
-public class KitchenObjectSO : ScriptableObject
-{
-    public string ObjName;
-    public Sprite ObjSprite;
-    public Transform ObjPrefab;
-}
